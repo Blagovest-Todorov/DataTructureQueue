a8947be50038b7cd919f7d144e62925739d5c3a7
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-12-04 15:26:21 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace _03.Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Queue<string> queue = new Queue<string>(); //creating a queue object 
+            queue.Enqueue("Message One");
+            queue.Enqueue("Message two");
+            queue.Enqueue("Message Three");
+            queue.Enqueue("Message Four");
+
+            while (queue.Count > 0) //till there is elements into the queue
+            {
+                string message = queue.Dequeue(); //removes the first elemetn of the  Queue
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
